0b68118864e8888b753e5af1fd1e0d148f886a53
bali-ruchita <166810976+bali-ruchita@users.noreply.github.com>
2025-10-18 23:02:25 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Ruchi@2004',
+  password: 'abcdef@123',
   database: 'bookdb'
 });
 
